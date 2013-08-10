2449ec58337789bbbf10d0f9cfaef6b295b88fc2
Aaron Hayes <aaron.hayes@uqconnect.edu.au>
2013-08-10 20:35:48 +1000

    Action Listeners for Buttons

--- Interface/src/ui/panels/control/ButtonPanel.java
+++ Interface/src/ui/panels/control/ButtonPanel.java
@@ -1,6 +1,8 @@
 package ui.panels.control;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Panel to display button controls
@@ -22,8 +24,42 @@ public class ButtonPanel extends JPanel {
         save = new JButton("Save Image");
         panorama = new JButton("Panorama");
 
+        setupListeners();
         add(stream);
         add(save);
         add(panorama);
     }
+
+    /**
+     *  Add Action Listeners To Each Button
+     */
+    private void setupListeners() {
+        ActionListener a1 = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Stream!!");
+
+            }
+        };
+
+        ActionListener a2 = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Save!!");
+            }
+        };
+
+        ActionListener a3 = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("panorama!!");
+            }
+        };
+
+
+        stream.addActionListener(a1);
+        save.addActionListener(a2);
+        panorama.addActionListener(a3);
+    }
 }
+
